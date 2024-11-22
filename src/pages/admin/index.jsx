30eb47ffba184ab54ad9947b00b13f2e3e71f0a0
iamthedankness <59592611+iamthedankness@users.noreply.github.com>
2024-11-22 23:52:09 +0500

    admin url

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -4,7 +4,7 @@ const Admin = () => {
   return (
     
     <div className='h-screen w-full flex justify-center items-center ' >
-           <Link className='bg-blue-500 text-white px-6 py-2 rounded-xl' to="http://127.0.0.1:8000/admin/" > LOGIN </Link>
+           <Link className='bg-blue-500 text-white px-6 py-2 rounded-xl' to="https://fusionflare-02ef9f77a95d.herokuapp.com/admin/" > LOGIN </Link>
      
         
     </div>
